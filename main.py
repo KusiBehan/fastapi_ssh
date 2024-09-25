7f324bc0fcc6c47368883a52f4d124cb4c993a32
Ubuntu <ubuntu@m321-18-ap21d.maas>
2024-09-25 14:21:17 +0000

    init commit setting up test api

---
+++ main.py
@@ -0,0 +1,17 @@
+from typing import Union
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+#Befehl um backend zum starten
+# uvicorn main:app --host 10.3.32.18 --port 8000    
